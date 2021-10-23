07dfc200d52d70e100dc862af34eb36fc9d0cc81
Mihir Kestur <mihirkestur@outlook.com>
2021-10-23 22:48:37 +0530

    update on ui

--- frontend.py
+++ frontend.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import os
 import cv2
+import search
 st.set_page_config(page_title='Upload Image', layout = 'centered', initial_sidebar_state = 'auto')
-st.title("RaaS: Recipe as a service")
+st.title("RaaS: Recipes as a service")
 take_pic = st.button("Take Picture")
 FRAME_WINDOW = st.image([])
 camera = cv2.VideoCapture(0)
 image_file = st.file_uploader("Upload A File",type=['png','jpeg','jpg'])
+def predict():
+    st.write("prediction should happen here")
+    
 if image_file is not None:
     file_details = {"FileName":image_file.name,"FileType":image_file.type}
     filetype = {image_file.type}
@@ -27,3 +31,12 @@ if(take_pic):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(frame)
     cv2.imwrite("test.jpg", frame)
+
+if(st.button("RaaS it")):
+    predict()
+    ingredients = ["apple"]
+    recipes = search.get_recipes(ingredients)
+    for i in recipes:
+        st.write("**Recipe name: **", str(i["recipe"]))
+        st.write("**Prep time: **", str(i["preptime"]))
+        st.write("**Cook time: **",  str(i["cooktime"]))
